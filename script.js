1d3376309db1b502fd5f021d448df411130e0fea
Dalmon Lima <ozeiasdsl@gmail.com>
2019-05-17 20:53:44 -0300

    loading comments

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
-console.log("teste");
+$(document).ready(function(){
 
-function getRandomSuggestion() {
-    return (Math.floor(Math.random() * 4) +1);
-}
+  function getRandomSuggestion() {
+      return (Math.floor(Math.random() * 4) +1);
+  }
 
-function suggest(){
-var rdm = getRandomSuggestion();
-console.log(rdm);
-
-$.ajax({
-    url: 'json/suggestion.json',
-    type: 'GET',
-    processData: false,
-    contentType: false,
-    dataType: 'json',
-    success: function(data){
-    console.log(data.suggestion[rdm]);
-    correctName = data.suggestion[rdm];
-    },
-    error: function(e) {
-      console.log('Error!', e);
+  function update() {
+    var element = document.getElementById("offer-progress");
+    var width = 1;
+    var identity = setInterval(scene, 1000);
+    function scene() {
+      if (width >= 100) {
+        clearInterval(identity);
+      } else {
+        width++;
+        element.style.width = width + '%';
+      }
     }
+  }
+
+  //Make a question
+  function loadComment(){
+    var author, title, text;
+
+    $.ajax({
+      url: 'json/comments.json',
+      type: 'GET',
+      processData: false,
+      contentType: false,
+      dataType: 'json',
+      success: function(data){
+
+        for (var i = 0; i < data.length; i++) {
+          author = data[i].author;
+          title = data[i].title;
+          text = data[i].text;
+          review = 'product-review' + i;
+
+
+          $('#comments').append('<div id="'+review+'"><div class="product-review-heading"><div class="review-header"><h4 class="product-review-title"></h4><span class="product-review-period"></span></div><span class="product-review-author"></span><div><span data-icon="*"></span><span data-icon="*"></span><span data-icon="*"></span><span data-icon="*"></span><span data-icon="*"></span></div></div><div class="product-review-post"><p class="product-review-text"></p></div><hr></div>')
+
+          $('#' + review + ' .product-review-author').html(author);
+          $('#' + review + ' .product-review-title').html(title);
+          $('#' + review + ' .product-review-text').html(text);
+        }
+
+      },
+      error: function(e) {
+          console.log('Error!', e);
+      }
     });
-    console.log( rdm );
-    $('#picture>img').remove();
-    $('#picture').append('<img src="imagens/normal/'+rdm+'.png" />');
-}
+  }
+
+  loadComment();
+
+  update();
+
+});
