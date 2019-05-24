e92bdff419f202c15755708c4d3aa54f5cbb2d1f
Dalmon Lima <ozeiasdsl@gmail.com>
2019-05-24 19:42:25 -0300

    update modifications

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
       success: function(data){
 
         reviewCount = data.length;
+        
         $('.reviewCount').append(reviewCount);
 
         for (var i = 0; i < data.length; i++) {
@@ -41,7 +42,7 @@ $(document).ready(function(){
           review = 'product-review' + i;
 
 
-          $('#comments').append('<div id="'+review+'" class="comment"><div class="product-review-heading"><div class="review-header row"><div class="col-sm-12 col-md-12 col-lg-6 col-xl-6"><h5 class="product-review-title"></h5></div><div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 comment-note"><span><i class="fas fa-star"></i></span><span><i class="fas fa-star"></i></span><span><i class="fas fa-star"></i></span><span><i class="fas fa-star"></i></span><span><i class="far fa-star"></i></span></div></div></div><div class="product-review-post"><p class="product-review-text"></p></div><div class="comment-footer"><div id="user"><div class="user-image"><span></span></div><div class="user-info"><div><span class="product-review-author"></span></div><div><span class="product-review-period"></span></div></div></div><div><span><a href="">Like</a></span><span><a href="">Dislike</a></span></div></div></div>')
+          $('#comments').append('<div id="'+review+'" class="comment"><div class="product-review-heading"><div class="review-header row"><div class="col-sm-12 col-md-12 col-lg-6 col-xl-6"><h5 class="product-review-title"></h5></div><div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 comment-note"><span><i class="fas fa-star"></i></span><span><i class="fas fa-star"></i></span><span><i class="fas fa-star"></i></span><span><i class="fas fa-star"></i></span><span><i class="far fa-star"></i></span></div></div></div><div class="product-review-post"><p class="product-review-text"></p></div><div class="comment-footer"><div id="user"><div class="user-image"><span></span></div><div class="user-info"><div><span class="product-review-author"></span></div><div><span class="product-review-period"></span></div></div></div><span class="opinion"><span><a href=""><i class="fas fa-thumbs-up"></i></a><span>5</span></span><span><a href=""><i class="fas fa-thumbs-down"></i></a><span>5</span></span></span></div></div>')
 
           $('#' + review + ' .product-review-author').html(author);
           $('#' + review + ' .product-review-title').html(title);
@@ -70,10 +71,22 @@ $(document).ready(function(){
   function colorUpdate(){
     $( ".colorPicker" ).click(function() {
       $('.colorPicker').removeClass('active');
+      var newColor = $(this).find("a").attr('data-color');
+      $(this).addClass('active');
+      $('#actual-color').html(newColor);
+      return false;
+    });
+  }
+
+  function filterUpdate(){
+    $( ".star-filter" ).click(function() {
+      $('.star-filter').removeClass('active');
       $(this).addClass('active');
       return false;
     });
   }
+
+  filterUpdate();
 
   colorUpdate();
 
